91e189d735b30eb58ee84f017fdf36f5187c763c
Lucia Brammer <brammerl96@gmail.com>
2020-06-16 14:16:47 -0700

    user post route and test

---
+++ __tests__/user.test.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+const User = require('../lib/models/User');
+
+describe('auth routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('can sign up user via POST', () => {
+    return request(app)
+      .post(`/api/v1/auth/signup`)
+      .send({
+        email: 'test@gmail.com',
+        password: 'testpw'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          email: 'test@gmail.com'
+        });
+      });
+  });
+});
